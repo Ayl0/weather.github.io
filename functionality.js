830d3000ba04b920ff09063bd03a854a33267931
Ayl0 <108296514+Ayl0@users.noreply.github.com>
2022-07-10 21:25:15 +0500

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -3,7 +3,7 @@ $(document).ready(()=>{
     form.submit( function(e) {
         e.preventDefault()
         const location = $(city).val()
-        const url = 'http://api.weatherapi.com/v1/current.json?key=b6b069d6adc84b8ab51141132221007&q=' + location + '&aqi=yes'
+        const url = 'https://api.weatherapi.com/v1/current.json?key=b6b069d6adc84b8ab51141132221007&q=' + location + '&aqi=yes'
         $.get(url, function(response){
             $("#weather-page").css("box-shadow", "0px 10px 30px 0px")
             $("#weather-page").css("height", "fit-content")
